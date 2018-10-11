1a99682bfe33cc4b0d323729a9eae181cd7b1773
Edu Wass <eduwass@gmail.com>
2018-10-11 08:02:59 +0200

    Add a mobile menu

--- wp-content/themes/barbershop/resources/assets/scripts/routes/common.js
+++ wp-content/themes/barbershop/resources/assets/scripts/routes/common.js
@@ -1,8 +1,28 @@
 export default {
   init() {
-    // JavaScript to be fired on all pages
+    // JavaScript to be fired on all pages, after page specific JS is fired
   },
   finalize() {
-    // JavaScript to be fired on all pages, after page specific JS is fired
+    // JavaScript to be fired on all pages
+    var whiteText = $('header.banner').hasClass('white-text');
+    $('.menu .hamburger').toggle(function() {
+      $('.mobile-menu').addClass('visible');
+      if(!whiteText){
+        $('header.banner').addClass('white-text');
+      }
+      $('html, body').css({
+          overflow: 'hidden',
+          height: '100%',
+      });
+    }, function() {
+      $('.mobile-menu').removeClass('visible');
+      if(!whiteText){
+        $('header.banner').removeClass('white-text');
+      }
+      $('html, body').css({
+          overflow: 'auto',
+          height: 'auto',
+      });
+    });
   },
 };
--- wp-content/themes/barbershop/resources/views/partials/header-menu.blade.php
+++ wp-content/themes/barbershop/resources/views/partials/header-menu.blade.php
@@ -3,8 +3,12 @@ $menu_locations = get_nav_menu_locations();
 $menu_items = wp_get_nav_menu_items( $menu_locations['header_menu'] );
 $main_logo = get_field('main_logo', 'option');
 $mobile_logo = get_field('mobile_logo', 'option');
+$hamburger_logo = get_field('hamburger_logo', 'option');
 @endphp
 <div class="row menu">
+  <div class="hamburger hidden-desktop">
+    <img class="hamburger-logo" src="{{$hamburger_logo['url']}}">
+  </div>
   <div class="col-3 col-lg-3 header-left hidden-mobile">
     @if(isset($menu_items[0])) <a class="menu-item" href="{{ $menu_items[0]->url}}">{{ $menu_items[0]->title }}</a> @endif
     @if(isset($menu_items[1])) <a class="menu-item" href="{{ $menu_items[1]->url}}">{{ $menu_items[1]->title }}</a> @endif
@@ -25,4 +29,11 @@ $mobile_logo = get_field('mobile_logo', 'option');
     @if(isset($menu_items[2])) <a class="menu-item" href="{{ $menu_items[2]->url}}">{{ $menu_items[2]->title }}</a> @endif
     @if(isset($menu_items[3])) <a class="menu-item" href="{{ $menu_items[3]->url}}">{{ $menu_items[3]->title }}</a> @endif
   </div>
+</div>
+<div class="mobile-menu hidden-desktop">
+  <nav class="nav-primary">
+    @if (has_nav_menu('header_menu'))
+      {!! wp_nav_menu(['theme_location' => 'header_menu', 'menu_class' => 'nav']) !!}
+    @endif
+  </nav>
 </div>
