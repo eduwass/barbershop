5f2d89d7a96b1dd16772c2d33d887888f5d4788e
Edu Wass <eduwass@gmail.com>
2018-10-11 20:26:43 +0200

    Add markup for Features/Staff blocks

--- wp-content/themes/barbershop/resources/views/partials/homepage-features.blade.php
+++ wp-content/themes/barbershop/resources/views/partials/homepage-features.blade.php
@@ -1,11 +1,15 @@
 <div class="row features">
-  <div class="col-12 col-lg-4">
-    1
-  </div>
-  <div class="col-12 col-lg-4">
-    2
-  </div>
-  <div class="col-12 col-lg-4">
-    3
-  </div>
+  @foreach(get_field('features') as $feature)
+    <div class="col-12 col-lg-4 feature-item">
+      <div class="image">
+        <img src="{{ $feature['image']['url'] }}">
+      </div>
+      <div class="title">
+        {{ $feature['title'] }}
+      </div>
+      <div class="text">
+        {{ $feature['text'] }}
+      </div>
+    </div>
+  @endforeach
 </div>
--- wp-content/themes/barbershop/resources/views/partials/homepage-staff.blade.php
+++ wp-content/themes/barbershop/resources/views/partials/homepage-staff.blade.php
@@ -1,11 +1,22 @@
+<h1>{{ get_field('staff_title') }}</h1>
 <div class="row staff">
-  <div class="col-12 col-lg-4">
-    1
-  </div>
-  <div class="col-12 col-lg-4">
-    2
-  </div>
-  <div class="col-12 col-lg-4">
-    3
-  </div>
+  @foreach(get_field('staff') as $staff)
+    <div class="col-12 col-lg-4 staff-item">
+      <div class="image">
+        <img src="{{ $staff['picture']['url'] }}">
+      </div>
+      <div class="name">
+        {{ $staff['name'] }}
+      </div>
+      <div class="description">
+        {{ $staff['description'] }}
+      </div>
+      <div class="years_of_experience">
+        {{ $staff['years_of_experience'] }}
+      </div>
+      <div class="email">
+        {{ $staff['email'] }}
+      </div>
+    </div>
+  @endforeach
 </div>
